d037a05c0bd19b818609b0a62fd3aa24a821282b
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-06-02 23:09:11 +0100

    update 6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,10 +3,10 @@
 
 int main()
 {
-  printf("Size of a char: %zu \n", sizeof(char));
-  printf("Size of an int: %ld \n", sizeof(int));
-  printf("Size of a long: %ld \n", sizeof(long int));
-  printf("Size of a long long: %ld\n", sizeof(long double));
-  printf("Size of a float: %ld \n", sizeof(float));
-  return (0);
+    printf("Size of a char: %zu \n", sizeof(char));
+    printf("Size of an int: %ld \n", sizeof(int));
+    printf("Size of a long: %ld \n", sizeof(long int));
+    printf("Size of a long long: %ld\n", sizeof(long double));
+    printf("Size of a float: %ld \n", sizeof(float));
+    return (0);
 }
