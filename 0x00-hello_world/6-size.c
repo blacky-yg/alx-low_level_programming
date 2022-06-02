1ac0fe6f9a92d1abb77c180f91a657041050047e
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-06-02 23:35:12 +0100

    upd

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@ int main(void)
 	printf("Size of an int: %ld byte(s)\n", sizeof(int));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 	if (sizeof(void *) == 4)
-		printf("Size of a long long int: %ld byte(s)\n", sizeof(long double));
+		printf("Size of a long long int: %ld byte(s)\n", sizeof(unsigned long int));
 	else
 		printf("Size of a long long int: %ld byte(s)\n", sizeof(long int));
 	printf("Size of a float: %ld byte(s)\n", sizeof(float));
