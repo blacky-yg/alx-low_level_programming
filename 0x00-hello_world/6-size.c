fc7e53927078a6806218e3382de8226c0217b46b
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-06-02 22:49:08 +0100

    Add scripts to compile c files

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Size of a char: %d \n", sizeof(char));
+  printf("Size of an int: ‰d \n", sizeof(int));
+  printf("Size of a long: ‰d \n", sizeof(long));
+  printf("Size of a long long: ‰lu\n", sizeof(long long));
+  printf("Size of a float: ‰lu\n", sizeof(float));
+  return (0);
+}
