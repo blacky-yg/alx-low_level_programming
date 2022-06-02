4e90ebe66e9727d9e87d0725bcd3d5275c12bda6
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-06-02 23:15:50 +0100

    update 6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <limits.h>
 
-int main()
+int main(void)
 {
-    printf("Size of a char: %zu \n", sizeof(char));
-    printf("Size of an int: %ld \n", sizeof(int));
-    printf("Size of a long: %ld \n", sizeof(long int));
-    printf("Size of a long long: %ld\n", sizeof(long double));
-    printf("Size of a float: %ld \n", sizeof(float));
-    return (0);
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long: %ld byte(s)\n", sizeof(unsigned long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
 }
