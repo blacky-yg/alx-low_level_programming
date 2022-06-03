e63f8dc601e31f8cc1505a4246d0b240eb6049c5
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-06-03 16:40:24 +0100

    update last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,9 +17,9 @@ int main(void)
     last_digit = n % 10;
 
     if (last_digit < 5)
-        printf("Last digit of %i is %i and is less than 6 and not 0\n", n);
+        printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last_digit);
     else if (last_digit > 5)
-        printf("Last digit of %i is %i and is greater than 5\n", n);
+        printf("Last digit of %i is %i and is greater than 5\n", n, last_digit);
     else
         printf("%i is zero\n", n);
     return (0);
