eb1a6413635ab797b554a8b8db44a88ff831878c
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-06-03 16:38:50 +0100

    adding 1 and 2 exo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 /**
- * main: print positive or negative numbers
- * void
- * return 0
+ * main - main block
+ * Description: Random number and print the number depending
+ * his size
+ * Return: 0
  */
 int main(void)
 {
+    int last_digit;
+    int n;
+
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    last_digit = n % 10;
+
+    if (last_digit < 5)
+        printf("Last digit of %i is %i and is less than 6 and not 0\n", n);
+    else if (last_digit > 5)
+        printf("Last digit of %i is %i and is greater than 5\n", n);
+    else
+        printf("%i is zero\n", n);
     return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: display alphabet letter from a to z
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	for (; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
