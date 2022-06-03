92709e64450d7e30fec0faa5c694b94695931de6
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-06-03 17:18:39 +0100

    update

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-    char c = '0';
+	char c = '0';
 
-    for (; c <= '9'; c++)
+	for (; c <= '9'; c++)
 		putchar(c);
-    c = 'a';
-    for (; c <= 'f'; c++)
+	c = 'a';
+	for (; c <= 'f'; c++)
 		putchar(c);
 	putchar('\n');
-    return (0);
+	return (0);
 }
