c054a4b5deb05af1e242447c7d48d649c809a42f
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-06-08 14:21:41 +0100

    update isalpha function

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
 /**
- * _isalpha: check function
+ * _isalpha - check function
  * @c: num passed as parameter
  *
  * Description: Check if a letter is alphanum or not
@@ -9,7 +9,7 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90) || (c >= 48 && c <= 57))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 		return (1);
 	return (0);
 }
