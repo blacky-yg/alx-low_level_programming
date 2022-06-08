2e8482fc149bf2890be5099b762e8a3d3e5de14c
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-06-08 16:23:50 +0100

    add 1à-add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add function
+ * @num1: first number to add
+ * @num2: second number to add
+ *
+ * Description: add to nums
+ * Return: the sum of the 2 nums
+ */
+int add(int num1, int num2)
+{
+    return (num1 + num2);
+}
