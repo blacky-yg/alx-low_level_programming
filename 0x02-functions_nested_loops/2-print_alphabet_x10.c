d3f26c78c94dd2efa5c0813994965c6cd9df6e79
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-06-08 13:29:47 +0100

    update print_alphabet x 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,11 @@
  */
 void print_alphabet_x10(void)
 {
-	for (int c = 0; c < 10; c++)
+	int i, c;
+
+	for (c = 0; c < 10; c++)
 	{
-		for (int i = 97; i <= 122; i++)
+		for (i = 97; i <= 122; i++)
 			putchar(i);
 		putchar('\n');
 	}
