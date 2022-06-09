47f0813f0591a23baf6c0d951e7e7b666323af38
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-06-09 16:41:28 +0100

    add print_diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,13 +13,14 @@ void print_diagonal(int n)
 
     if (n > 0)
     {
-        for (i = 1; i <= n; i++)
+        for (i = 0; i < n; i++)
         {
-            _putchar('\\');
-            _putchar('\n');
             for (j = 0; j < i; j++)
                 _putchar(' ');
+            _putchar('\\');
+            _putchar('\n');
         }
     }
-    _putchar('\n');
+    else
+        _putchar('\n');
 }
