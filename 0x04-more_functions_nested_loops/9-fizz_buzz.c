981194ed25d865bc2511ee454f79e604fd7cc88f
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-06-09 17:01:57 +0100

    update

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * fizz_buzz - fizz Buzz program
+ *
+ * Description: Fizz Buzz (3 && 5)
+ * Return: 0
+ */
+int main(void)
+{
+    int i;
+
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+            printf("Fizz Buzz");
+        else if (i % 3 == 0)
+            printf("Fizz");
+        else if (i % 5 == 0)
+            printf("Buzz");
+        else
+            printf("%d", i);
+        if (i != 100)
+            printf(" ");
+    }
+    return (0);
+}
