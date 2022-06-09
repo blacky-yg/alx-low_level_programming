0696734573a562532a4373198254856c854265cb
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-06-09 14:44:45 +0100

    update

---
+++ template.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * main - main block
+ * @ac
+ * @av
+ * Description:
+ * Return: 0 by default
+ */
+int main(int ac, char *av[])
+{
+    return (0);
+}
