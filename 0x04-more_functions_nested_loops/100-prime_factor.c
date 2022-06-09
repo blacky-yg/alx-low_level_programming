d6e607c91891a07f22d48a42f3995d83c197465f
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-06-09 17:29:01 +0100

    update

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - largest prime of 612852475143
+ *
+ * Description: get largest prime of this number
+ * Return: Success Always
+ */
+int main(void)
+{
+	long int x = 612852475143;
+	long int y;
+
+	for (y = 2; y < x; y++)
+		if (x % y == 0)
+			x = x / y;
+	printf("%ld\n", y);
+	return (0);
+}
