6e09793120ec6cc8622500b7eb27908bf5ecb69e
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-06-09 13:33:21 +0100

    add first exercise

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - main block
+ * Description: Test the function pos_or_neg
+ * Return: 0 by default
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+    return (0);
+}
