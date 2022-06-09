e953abf048d907a811222741fd9461fdee0aab53
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-06-09 15:47:52 +0100

    adding 2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiply 2 nums ?
+ * @a: first num
+ * @b: second num
+ *
+ * Description: Multiply 2 numbers
+ * Return: the result of the mul of the 2 nums
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
