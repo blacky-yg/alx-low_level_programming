03e27959a5f94b869d24a269b43d2094637443a9
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-06-09 17:18:20 +0100

    update 10-print_triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,12 +8,17 @@
 void print_triangle(int size)
 {
 	int i;
+	int n;
 	int j;
 
-	for (i = 1; i <= size; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 1; j <= i; j++)
+		for (j = size - 1 - i; j > 0; j--)
+			_putchar(' ');
+		for (n = 0; n <= i; n++)
 			_putchar('#');
 		_putchar('\n');
 	}
+	if (size <= 0)
+		_putchar('\n');
 }
