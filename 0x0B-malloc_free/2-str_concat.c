43f68a8894360abb30f8810e50df43d243200e92
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-06-27 19:56:31 +0100

    update str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -26,17 +26,26 @@ int _strlen(char *s)
  */
 char *str_concat(char *s1, char *s2)
 {
-	int len_s1 = _strlen(s1);
-	int len_s2 = _strlen(s2);
-	char *final_str = malloc(len_s1 + len_s2 + 1);
-	int i;
+	int len_s1 = 0;
+	int len_s2 = 0;
+	char *final_str = NULL;
+	int i, j;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	len_s1 = _strlen(s1);
+	len_s2 = _strlen(s2);
+	final_str = malloc(sizeof(char) * (len_s1 + len_s2 + 1));
 	if (final_str == NULL)
+	{
+		free(final_str);
 		return (NULL);
-	for (i = 0; s1[i]; i++)
+	}
+	for (i = 0; i < len_s1; i++)
 		final_str[i] = s1[i];
-	for (i = 0; s2[i]; i++, len_s1++)
-		final_str[len_s1] = s2[i];
-	final_str[len_s1 + len_s2] = '\0';
+	for (j = 0; j <= len_s2; i++, j++)
+		final_str[i] = s2[j];
 	return (final_str);
 }
