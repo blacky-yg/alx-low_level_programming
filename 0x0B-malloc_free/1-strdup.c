2aa0c5aff88b4c075e82b7799c948e30dc33253d
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-06-27 19:09:01 +0100

    update strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,7 +12,7 @@ char *_strdup(char *str)
 	int i, len_str;
 	char *dup_str;
 
-	if (str == NULL || str == "")
+	if (str == NULL)
 		return (NULL);
 	for (len_str = 0; str[len_str]; len_str++)
 	{}
@@ -21,8 +21,7 @@ char *_strdup(char *str)
 	dup_str = malloc(len_str + 1);
 	if (dup_str == NULL)
 		return (NULL);
-	for (i = 0; i < len_str; i++)
+	for (i = 0; i <= len_str; i++)
 		dup_str[i] = str[i];
-	dup_str[i] = '\0';
 	return (dup_str);
 }
