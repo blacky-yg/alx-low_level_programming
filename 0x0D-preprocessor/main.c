5760c6037ee80ed9a9354c721a52e9314de53d68
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-07-04 19:47:20 +0100

    add main functions for two days

---
+++ 0x0D-preprocessor/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "4-sum.h"
+#include "4-sum.h"
+#include <stdio.h>
+#include "3-function_like_macro.h"
+#include "3-function_like_macro.h"
+#include "1-pi.h"
+#include "0-object_like_macro.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    // int s;
+
+    // s = SUM(98, 1024);
+    // printf("%d\n", s);
+    // int i;
+    // int j;
+
+    // i = ABS(-98) * 10;
+    // j = ABS(98) * 10;
+    // printf("%d, %d\n", i, j);
+    // float a;
+    // float r;
+
+    // r = 98;
+    // a = PI * r * r;
+    // printf("%.3f\n", a);
+
+    int s;
+
+    s = 98 + SIZE;
+    printf("%d\n", s);
+    return (0);
+}
