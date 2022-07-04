bc196a236f6b406da86e968b85618fb5ff404eaf
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-07-04 19:32:19 +0100

    update new_dog function

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,17 +16,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *d;
 
 	d = malloc(sizeof(dog_t));
-	if (!d)
-	{
-		return (NULL);
-	}
 	name_cpy = strdup(name);
 	owner_cpy = strdup(owner);
-	if (name_cpy && owner_cpy)
+	if (d && name_cpy && owner_cpy)
 	{
 		d->age = age;
 		d->name = name_cpy;
 		d->owner = owner_cpy;
+	}
+	else
+	{
+		free(d);
+		free(name_cpy);
+		free(owner_cpy);
+		return (NULL);
 	}
 	return (d);
 }
