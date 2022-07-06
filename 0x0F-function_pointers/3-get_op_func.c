151ec69ed0bcb5f9d133307f9fcc7b2151b1ab72
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-07-06 18:12:11 +0100

    update operations functions

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,14 +9,17 @@
 */
 int (*get_op_func(char *s))(int, int)
 {
-	int i;
 	op_t ops[] = {
-		{"+", op_add}, {"-", op_sub},
-		{"*", op_mul}, {"/", op_div},
-		{"%", op_mod}, {NULL, NULL}
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
+	int i = 0;
 
-	for (i = 0; ops[i].op && *(ops[i].op) != *s; i++)
+	for (; ops[i].op && *(ops[i].op) != *s; i++)
 	{}
 	return (ops[i].f);
 }
