197fad011c8edebb6a7527aaf51e400703b97eab
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-07-07 15:26:02 +0100

    update

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,11 +11,10 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	int sum = 0;
 	va_list args;
 
 	if (n == 0)
-		return (0);
+		return;
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
