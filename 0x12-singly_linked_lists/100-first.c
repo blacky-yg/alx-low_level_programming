0d2a57df8f0c90a1e44caac2f19d48a9434135d9
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-07-15 13:34:20 +0100

    adding advanced exercise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * launch_first - executed before the main function
+ *
+ * Return: void
+ */
+void launch_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
