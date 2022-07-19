c74af5088aa189b440e5ee2ca595cb24a7e89317
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-07-19 17:47:06 +0100

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,17 +2,16 @@
 
 /**
  * print_listint - Print all elements of a list
- * @h: linked_list
+ * @h: linked list
  *
  * Return: number of node
  */
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *tmp;
-	size_t nb;
+	const listint_t *tmp = h;
+	size_t nb = 0;
 
-	tmp = h;
-	for (nb = 0; tmp; tmp = tmp->next, nb++)
+	for (; tmp; tmp = tmp->next, nb++)
 		printf("%d\n", tmp->n);
 	return (nb);
 }
