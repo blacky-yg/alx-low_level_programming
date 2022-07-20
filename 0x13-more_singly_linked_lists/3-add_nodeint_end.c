fa533d49d4576d1e0b5916a692ef4445809b1022
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-07-20 12:13:53 +0100

    update

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -16,7 +16,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
     if (!new_node)
         return (NULL);
     new_node->n = n;
-    new_node->next = *head;
+    new_node->next = NULL;
     if (tmp)
     {
         for (; tmp->next; tmp = tmp->next)
