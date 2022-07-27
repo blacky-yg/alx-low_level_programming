344de328047a906c4d630f1269f57d255ca6efaa
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-07-27 02:38:33 +0100

    Update checking errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,18 +10,25 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd = 0, i;
-	size_t nb_chars = 0;
+	int nb_chars = 0;
 	char *content = NULL;
 
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (fd < 0)
 		return (0);
-	content = malloc(letters + 1);
+	content = malloc(letters);
+	if (!content)
+		return (0);
 	nb_chars = read(fd, content, letters);
-	if (nb_chars == (size_t)-1)
+	if (nb_chars < 0)
+	{
+		free(content);
 		return (0);
+	}
+	content[nb_chars] = '\0';
+	close(fd);
 	for (i = 0; content[i]; i++)
 		write(STDOUT_FILENO, &content[i], 1);
 	free(content);
