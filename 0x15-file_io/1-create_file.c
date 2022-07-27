9d62f4173d3e7fc1a658d886c386d0bec16dfaab
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-07-27 02:49:30 +0100

    update create_file function

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,16 +9,20 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd = 0, i;
+	int fd = 0, i, nb_chars;
 
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, "u+rw");
-	if (fd == -1)
+	if (fd < 0)
 		return (-1);
 	if (!text_content)
 		return (-1);
 	for (i = 0; text_content[i]; i++)
-		write(fd, &text_content[i], 1);
+	{}
+	nb_chars = write(fd, text_content, i);
+	if (i != nb_chars)
+		return (-1);
+	close(fd);
 	return (1);
 }
